48607f011e4851cf4f03e3e3266f312399feb75b
MicaelaPla <83314344+MicaelaPla@users.noreply.github.com>
2021-05-21 15:31:31 -0300

    Add files via upload

---
+++ phpp.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+<?php
+
+   $nom= $_POST["Nombre"];
+   $cont= $_POST["contraseña"];
+
+
+   if ($nom== "SEPTIEMO" & $cont== "Murialdo12")
+   {
+   echo "Entraste al sistema";
+   }
+
+   else
+   {
+   echo "Error";
+   }
+
+?>
+</body>
+</html>
+
